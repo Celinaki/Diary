c6d0c41e29edbfeef47137d1565976a11b29c837
Celinaki <celina1999@live.se>
2023-10-04 23:17:47 +0200

    Pagination skeleton

---
+++ src/components/icons/Pagination.vue
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <span>
+           &lt;
+        </span>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>></span>
+    </div>
+</template>
+
+<style scoped>
+div{
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    padding-bottom: 26px;
+
+}
+span{
+    border-radius: 4px;
+    padding: 4px 8px;
+    background-color: rgb(255, 255, 255);
+}
+</style>
