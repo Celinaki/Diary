4442cf23316aa0c8d90e724691ae36315167eb9a
Celinaki <celina1999@live.se>
2023-10-04 23:16:39 +0200

    Scrollable list for entries

--- src/views/DiaryView.vue
+++ src/views/DiaryView.vue
@@ -1,13 +1,65 @@
 <script setup>
-
+import EntryCard from '../components/icons/EntryCard.vue';
+import Pagination from '../components/icons/Pagination.vue'
+import CreateEntryButton from '../components/CreateEntryButton.vue'
 </script>
 
 <template>
-    <div>
-        
+    <div class="wrapper">
+        <h1>Entries</h1>
+        <section class="sortby-wrapper">
+            <h2>Sort by: </h2>
+            <select name="" id="sort-by">
+                <option value="Newest">Newest</option>
+                <option value="Oldest">Oldest</option>
+            </select>
+        </section>
+        <div class="entries-wrapper">
+            <EntryCard/>
+            <EntryCard/>
+            <EntryCard/>
+            <EntryCard/>
+            <EntryCard/>
+        </div>
+        <Pagination/>
     </div>
+    <CreateEntryButton/>
 </template>
 
-<style >
+<style scoped>
+template{
+    position: relative;
+}
+.wrapper{
+margin: 46px 37px;
+display: flex;
+flex-direction: column;
+}
+.wrapper h1{
+    margin: 0;
+    font-weight: 500;
+}
+.sortby-wrapper{
+    padding-top: 28px;
+    display: flex;
+    align-items: center;
+    align-self: flex-end;
+    gap: 8px;
+    border-radius: 25px;
+    
+}
+.sortby-wrapper select{
+    height: fit-content;
+    font-size: 14px;
+}
+
+.entries-wrapper{
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding-bottom: 38px;
+    padding-top: 25px;
+}
+
 
 </style>
