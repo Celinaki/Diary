3bd64048a24877d95a1e7130d31e8d0692eec04a
Celinaki <celina1999@live.se>
2023-10-04 23:20:36 +0200

    Skeleton for create entry button

---
+++ src/components/CreateEntryButton.vue
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<template>
+    <div>
+        <h2>Create an entry</h2>
+        <img src="@/assets/Edit.svg" alt="">
+    </div>
+</template>
+
+<style scoped>
+div{
+    border-top-right-radius: 40px;
+    border-top-left-radius: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* border-top: 1px solid #212427; */
+    height: 55px;
+    width: 100%;
+    background-color: #fff;
+    box-sizing: border-box;
+    position: sticky;
+    bottom: 0;
+    box-shadow: 0px -1px 3px 0px rgba(22, 22, 22, 0.25);
+
+}
+div img{
+    width: 28px;
+    height: 24px;
+    padding-left: 4px;
+}
+</style>
