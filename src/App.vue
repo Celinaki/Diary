088193b637b61340a3a61bfc042322cd92208e64
Celinaki <celina1999@live.se>
2023-10-05 16:59:58 +0200

    onmounted get all entries in app connected to store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <script setup>
 import DiaryView from './views/DiaryView.vue';
+import {storeToRefs} from 'pinia'
+import {ref, onMounted} from 'vue'
+import { useEntriesStore } from '@/stores/EntriesStore';
+
+const entriesStore = useEntriesStore()
+const { allEntries } = storeToRefs(entriesStore)
+
+
+
+onMounted(()=>{
+    
+    fetch('./src/assets/entries.json') 
+    .then(res => res.json())
+    .then(data => 
+    entriesStore.setAllEntries(data)
+    // allEntries.value = data
+    )
+
+
+
+})
+
+
 </script>
 
 <template>
