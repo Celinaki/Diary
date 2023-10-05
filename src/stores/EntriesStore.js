c70c2a939cf0bdc6f9ded62e2eb7893ae7c4f035
Celinaki <celina1999@live.se>
2023-10-05 18:42:52 +0200

    comments for logics entriesstore

--- src/stores/EntriesStore.js
+++ src/stores/EntriesStore.js
@@ -5,10 +5,14 @@ import { ref, computed } from 'vue'
 export const useEntriesStore = defineStore('entriesStore',() =>  {
 
     const allEntries = ref([])
-
+//Initial value of array
     const setAllEntries = (array) => {
         allEntries.value=array
     }
+//Initial value of array
+
+
+//Sort by date
     const sortEntries = (sortby) =>{
         const currentArray = allEntries.value
         if(sortby==='newest'){
@@ -39,21 +43,9 @@ export const useEntriesStore = defineStore('entriesStore',() =>  {
             })
             return allEntries=currentArray
         }
-        // if(sortby=='oldest'){
-        //     currentArray.sort((a,b) =>{
-        //         const dateA = new Date(a.date);
-        //         const dateB = new Date(b.date)
-        //         if(dateA<dateB){
-        //             return 1
-        //         }
-        //         if(dateA> dateB){
-        //             return -1
-        //         }
-        //         return 0
-        //     })
-        //     return allEntries.value=currentArray
-        // }
     }
+//Sort by date
+
 
     return {
         allEntries, setAllEntries, sortEntries
