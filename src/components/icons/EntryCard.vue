4bc4df249d0a187e282f800fd7c5f591cc9bf4d0
Celinaki <celina1999@live.se>
2023-10-06 13:04:06 +0200

    changed position of createEntryButton in code

--- src/components/icons/EntryCard.vue
+++ src/components/icons/EntryCard.vue
@@ -1,17 +1,16 @@
 <script setup>
 import { useDeleteEntryStore } from '@/stores/DeleteEntryStore';
-import {storeToRefs} from 'pinia'
+import { storeToRefs } from 'pinia'
 import { useEntriesStore } from '@/stores/EntriesStore';
-import {ref} from 'vue'
+import { ref } from 'vue'
 
-const props = defineProps(['entryDate','entryText','entryId'])
+const props = defineProps(['entryDate', 'entryText', 'entryId'])
 const store = useDeleteEntryStore()
 const { deleteEntryModal } = storeToRefs(store)
 const entriesStore = useEntriesStore()
 
 const passedId = ref(props.entryId)
 const openModal = () => {
-    console.log("hej här", deleteEntryModal.value)
     deleteEntryModal.value = true;
     entriesStore.getId(passedId.value)
 }
@@ -21,40 +20,39 @@ const openModal = () => {
     <div class="entry-wrapper">
         <span class="first-row">
             <h2> {{ entryDate }}</h2>
-            <img src="@/assets/clarity_trash-line.svg" 
-            @click="openModal" 
-            
-            alt="Trashcan icon">
+            <img src="@/assets/clarity_trash-line.svg" @click="openModal" alt="Trashcan icon">
         </span>
-  <p>{{ entryText }}</p>
+        <p>{{ entryText }}</p>
     </div>
 </template>
 
 <style scoped>
-.entry-wrapper{
-min-height: 136px;
-background-color: #FFF;
-border-radius: 6px;
-padding: 1rem;
-box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.20);
+.entry-wrapper {
+    min-height: 136px;
+    background-color: #FFF;
+    border-radius: 6px;
+    padding: 1rem;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.20);
 }
-.first-row{
-display: flex;
-justify-content: space-between;
-align-items: center;
+
+.first-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
-.first-row h2{
+
+.first-row h2 {
     margin: 6px 0px 13px 0px;
 }
-.first-row img{
-    width:18px;
+
+.first-row img {
+    width: 18px;
     height: 18px;
 }
-.entry-wrapper p{
+
+.entry-wrapper p {
     font-size: 12px;
     font-weight: 500;
     margin: 0;
 }
-
-
 </style>
